b2488280d906422b8c97c81b55c0ab9d393dca59
chli-jiseong <chlwltjd98@naver.com>
2021-10-05 16:06:19 +0900

    first commit

---
+++ RealTineChat/app/Http/Controllers/ChatController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChatMessage;
+use App\Models\ChatRoom;
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    /*
+        1. 채팅방 리스트 리턴
+        2. 특정 채팅방의 메시지 리스트 리턴
+        3. 특정 메시지 저장
+    */
+
+    public function rooms() {
+        return ChatRoom::all();
+    }
+
+    public function messages($roomId) {
+        $msgs =  ChatMessage::where('chat_room_id', $roomId)->with('user')->latest()->get(); //with절을 사용해서 관계된 사용자의 정보도 받아올 수 있다.
+        // $msgs[0]->user->name;
+        //lazy loading VS eager loading
+        dd($msgs);
+        return $msgs;
+    }
+
+    public function newMessage(Request $request, $roomId) {
+
+    }
+}
---
+++ RealTineChat/database/seeders/ChatRoomSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ChatMessage;
+use App\Models\ChatRoom;
+use Illuminate\Database\Seeder;
+
+class ChatRoomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        ChatRoom::create(['name' => 'WDJ반']);
+        ChatRoom::create(['name' => 'Laravel']);
+    }
+
+}
